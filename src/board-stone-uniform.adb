bfee9777b6b64c409412cdf7f383461f6be193a3
Christoph Welzel <welzel@in.tum.de>
2021-03-15 12:22:01 +0100

    a bit more setup for unique list

--- src/board-stone-uniform.adb
+++ src/board-stone-uniform.adb
@@ -15,10 +15,42 @@ package body Board.Stone.Uniform is
       return (Fac (N) / (Fac (K) + Fac(N - K)));
     end Bin;
 
-  Count_Result: constant Positive := (if Stone_Width /= Stone_Height
-                                      then Bin (2*Stone_Width + 2*Stone_Height, Connectors) / 2
-                                      else Bin (2*Stone_Width + 2*Stone_Height, Connectors) / 4);
-  Result: Stack_T (1 .. Count_Result);
+    Count_Result: constant Positive := (if Stone_Width /= Stone_Height
+                                        then Bin (2*Stone_Width + 2*Stone_Height, Connectors) / 2
+                                        else Bin (2*Stone_Width + 2*Stone_Height, Connectors) / 4);
+
+    Result: Stack_T (1 .. Count_Result);
+
+    Longest_Side: constant Positive := (if Stone_Width < Stone_Height
+                                        then Stone_Height
+                                        else Stone_Width);
+
+    subtype Side_Index_T is Positive range 1 .. Longest_Side;
+
+    type Side_T is new Border_T(Side_Index_T);
+
+    type Optional_Side_T (Empty: Boolean := True) is
+      record
+        case Empty is
+          when True => null;
+          when False => Side_T: Content;
+        end case;
+      end record;
+
+    type Side_Collection_T is array (Positive range <>) of Side_T;
+
+    function Smaller (Left: In Side_T;
+                      Right: In Side_T;
+                      Cmp_Index: In Side_Index_T) return Boolean is
+    begin
+      for I in 1 .. Cmp_Index loop
+        if Left(I) = Border_Connector_T.Open and Right(I) = Border_Connector_T.Closed then
+          return True;
+        end if;
+      end loop;
+      return False;
+    end Smaller;
+
 
   begin
     return result;
