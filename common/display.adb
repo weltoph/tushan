0cdeae54e3c12c37bdfbbfba2d21ac32ee13ebb4
Christoph Welzel <welzel@in.tum.de>
2021-12-15 19:45:19 +0100

    made presentation a little bit cuter2

--- common/display.adb
+++ common/display.adb
@@ -5,13 +5,12 @@ package body Display is
 
   function Display(Display_Board: Game_Board.Board_T) return Board_Display_T is
     Result: Board_Display_T := (others => (others => (' ')));
-    type Tile_Display is array (1 .. 5, 1 .. 5) of Wide_Character;
+    type Tile_Display is array (1 .. 3, 1 .. 3) of Wide_Character;
 
     function Render_Tile (Point: In Game_Board.Point_T) return Tile_Display is
       Tile: Tile_Display := (others => (others => ' '));
       use Game_Board;
     begin
-      Tile(3, 3) := 'x';
       if Game_Board.Is_Occupied(Display_Board, Point) then
         declare
           Northern: constant Game_Board.Inner_Connector_T := Game_Board.Get_Connector(Display_Board, Point, Game_Board.North);
@@ -20,44 +19,28 @@ package body Display is
           Western: constant Game_Board.Inner_Connector_T := Game_Board.Get_Connector(Display_Board, Point, Game_Board.West);
         begin
 
-          if Northern = Open then
-            Tile(3, 2) := '↑';
-          end if;
-
-          if Southern = Open then
-            Tile(3, 4) := '↓';
-          end if;
-
-          if Eastern = Open then
-            Tile(4, 3) := '→';
-          end if;
-
-          if Western = Open then
-            Tile(2, 3) := '←';
-          end if;
-
-          if Northern /= Inner then
+          if Northern = Closed then
             Tile(2, 1) := '─';
-            Tile(3, 1) := '─';
-            Tile(4, 1) := '─';
+          elsif Northern = Open then
+            Tile(2, 1) := '┴';
           end if;
 
-          if Eastern /= Inner then
-            Tile(5, 2) := '│';
-            Tile(5, 3) := '│';
-            Tile(5, 4) := '│';
+          if Eastern = Closed then
+            Tile(3, 2) := '│';
+          elsif Eastern = Open then
+            Tile(3, 2) := '├';
           end if;
 
-          if Southern /= Inner then
-            Tile(2, 5) := '─';
-            Tile(3, 5) := '─';
-            Tile(4, 5) := '─';
+          if Southern = Closed then
+            Tile(2, 3) := '─';
+          elsif Southern = Open then
+            Tile(2, 3) := '┬';
           end if;
 
-          if Western /= Inner then
+          if Western = Closed then
             Tile(1, 2) := '│';
-            Tile(1, 3) := '│';
-            Tile(1, 4) := '│';
+          elsif Western = Open then
+            Tile(1, 2) := '┤';
           end if;
 
           if Northern /= Inner then
@@ -68,9 +51,9 @@ package body Display is
             end if;
 
             if Eastern /= Inner then
-              Tile(5, 1) := '┐';
+              Tile(3, 1) := '┐';
             else
-              Tile(5, 1) := '─';
+              Tile(3, 1) := '─';
             end if;
           else
             if Western /= Inner then
@@ -78,32 +61,42 @@ package body Display is
             end if;
 
             if Eastern /= Inner then
-              Tile(5, 1) := '│';
+              Tile(3, 1) := '│';
             end if;
           end if;
 
           if Southern /= Inner then
             if Western /= Inner then
-              Tile(1, 5) := '└';
+              Tile(1, 3) := '└';
             else
-              Tile(1, 5) := '─';
+              Tile(1, 3) := '─';
             end if;
 
             if Eastern /= Inner then
-              Tile(5, 5) := '┘';
+              Tile(3, 3) := '┘';
             else
-              Tile(5, 5) := '─';
+              Tile(3, 3) := '─';
             end if;
           else
             if Western /= Inner then
-              Tile(1, 5) := '│';
+              Tile(1, 3) := '│';
             end if;
 
             if Eastern /= Inner then
-              Tile(5, 5) := '│';
+              Tile(3, 3) := '│';
             end if;
           end if;
         end;
+      else
+        Tile(1, 1) := '┏';
+        Tile(1, 2) := '┃';
+        Tile(1, 3) := '┗';
+        Tile(2, 1) := '━';
+        Tile(2, 2) := '░';
+        Tile(2, 3) := '━';
+        Tile(3, 1) := '┓';
+        Tile(3, 2) := '┃';
+        Tile(3, 3) := '┛';
       end if;
       return Tile;
     end Render_Tile;
@@ -113,11 +106,11 @@ package body Display is
         declare
           Point: constant Game_Board.Point_T := (X, Y);
           Tile: constant Tile_Display := Render_Tile(Point);
-          Ref_X: constant Natural := (X - 1)*5;
-          Ref_Y: constant Natural := (Y - 1)*5;
+          Ref_X: constant Natural := (X - 1)*3;
+          Ref_Y: constant Natural := (Y - 1)*3;
         begin
-          for Xp in 1 .. 5 loop
-            for Yp in 1 .. 5 loop
+          for Xp in 1 .. 3 loop
+            for Yp in 1 .. 3 loop
               Result(Ref_X + Xp, Ref_Y + Yp) := Tile(Xp, Yp);
             end loop;
           end loop;
