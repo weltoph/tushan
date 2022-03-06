682035895efaa06b4bd26de3a5a1ac4f11c622ea
Christoph Welzel <welzel@in.tum.de>
2022-03-06 13:38:21 +0100

    Game setup

--- common/game.adb
+++ common/game.adb
@@ -1,8 +1,13 @@
 package body Game is
-  protected body Game_State_T is
-    function Get_Board return Game_Board.Board_T is
-    begin
-      return Board;
-    end Get_Board;
-  end Game_State_T;
+  function Get_Player(Actor: Actor_T) return Player_Acc
+  is
+  begin
+    return Actor.Player;
+  end Get_Player;
+
+  function Connects(Actor: Actor_T; Direction: Game_Board.Direction_T) return Boolean
+  is
+  begin
+    return Actor.Target(Direction);
+  end Connects;
 end Game;
--- common/game.ads
+++ common/game.ads
@@ -4,8 +4,7 @@ with Ada.Containers.Vectors;
 generic
   with package Game_Board is new Board(<>);
 package Game is
-  type Player_Identifier is new Natural;
-  type Message_Coordinates is new Positive;
+  type Player_Identifier is new Positive;
 
   type Message_Type is (Game_Begin, Move_Request, Move_Broadcast, Game_End);
   type Message_Content (Msg_Type: Message_type := Game_Begin) is
@@ -31,23 +30,45 @@ package Game is
     (Index_Type => Positive,
      Element_Type => Message_Content);
 
-  type Answer_Content is
+  type Answer_Type is (Timeout, Success);
+  type Answer_Content(Msg_Type: Answer_Type := Success) is
     record
-      Rotation: Game_Board.Rotation_T;
-      Position: Game_Board.Point_T;
+      case Msg_Type is
+        when Timeout =>
+          null;
+        when Success => 
+          Rotation: Game_Board.Rotation_T;
+          Position: Game_Board.Point_T;
+      end case;
     end record;
 
   package Answer_List is new Ada.Containers.Vectors
     (Index_Type => Positive,
      Element_Type => Answer_Content);
 
-  protected Game_State_T is
-    function Get_Board return Game_Board.Board_T;
-    --  function Get_Player_Amount return Positive;
-    --  function Get_Sent_Messages(Player: Positive) return Message_List.Vector;
-    --  function Get_Recv_Messages(Player: Positive) return Message_List.Vector;
+  type Player_T is abstract tagged null record;
+
+  procedure Send_Msg(Player: In Player_T; Msg: In Message_Content) is abstract;
+  function Recv_Msg(Player: In Player_T) return Answer_Content is abstract;
+
+  type Player_Acc is access Player_T'Class;
+
+  type Actor_T is private;
+
+  function Get_Player(Actor: Actor_T) return Player_Acc;
+  function Connects(Actor: Actor_T; Direction: Game_Board.Direction_T) return Boolean;
+
   private
-    Board: Game_Board.Board_T;
-  end Game_State_T;
+
+  type Target_T is array (Game_Board.Direction_T) of Boolean;
+  type Actor_T is
+    record
+      Player: Player_Acc;
+      Target: Target_T;
+    end record;
+
+  package Actor_List is new Ada.Containers.Vectors
+    (Index_Type => Positive,
+     Element_Type => Actor_T);
 
 end Game;
